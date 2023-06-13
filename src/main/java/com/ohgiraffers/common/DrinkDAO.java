707fbccee8cd31a79cbca8e682f1623a23d24846
wanny <kko6369@naver.com>
2023-06-13 17:05:49 +0900

    fix: 재고 관리를 위해 수량 0으로 변경

--- src/main/java/com/ohgiraffers/common/DrinkDAO.java
+++ src/main/java/com/ohgiraffers/common/DrinkDAO.java
@@ -14,7 +14,7 @@ public class DrinkDAO {
         this.drinkMap = new HashMap<>();
 
         this.drinkMap.put("코카콜라", new Drink("코카콜라", 2000, 3));
-        this.drinkMap.put("밀키스", new Drink("밀키스",2500, 3));
+        this.drinkMap.put("밀키스", new Drink("밀키스",2500, 0));
         this.drinkMap.put("비락식헤", new Drink("비락식혜", 1500, 3));
     }
 
